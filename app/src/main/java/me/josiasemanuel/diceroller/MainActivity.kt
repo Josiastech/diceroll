8ff6cb41a03c5aefe7c09be40e5f7da11f071716
Josias <josiastech@gmail.com>
2019-10-21 22:49:15 -0600

    Set toast message

--- app/src/main/java/me/josiasemanuel/diceroller/MainActivity.kt
+++ app/src/main/java/me/josiasemanuel/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package me.josiasemanuel.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,10 @@ class MainActivity : AppCompatActivity() {
         // Click on button event
         val rollButon: Button = findViewById(R.id.roll_button)
         rollButon.text = "Let's roll"
+
+        // show a toas when button is clicked
+        rollButon.setOnClickListener{
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
